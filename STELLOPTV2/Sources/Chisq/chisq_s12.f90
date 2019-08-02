fb6a4809a49e4847761ac807ddb05127c55eff3e
Samuel Lazerson <lazerson@princeton.edu>
2019-08-02 16:47:38 +0200

    Added S12, S21 and S22 targets

---
+++ STELLOPTV2/Sources/Chisq/chisq_s12.f90
@@ -0,0 +1,72 @@
+!-----------------------------------------------------------------------
+!     Subroutine:    chisq_s12
+!     Authors:       S. Lazerson (lazerson@pppl.gov)
+!     Date:          08/02/2019
+!     Description:   Calculates the S12 susceptance matrix target.
+!                    For details see
+!                       Strand and Houlberg, Phys. Plasmas 2001, Vol 8,
+!                              No. 6, p. 2782
+!-----------------------------------------------------------------------
+      SUBROUTINE chisq_s12(target,sigma,niter,iflag)
+!-----------------------------------------------------------------------
+!     Libraries
+!-----------------------------------------------------------------------
+      USE stellopt_runtime
+      USE stellopt_targets
+      USE equil_utils
+      
+!-----------------------------------------------------------------------
+!     Input/Output Variables
+!
+!-----------------------------------------------------------------------
+      REAL(rprec), INTENT(in)    ::  target(nprof)
+      REAL(rprec), INTENT(in)    ::  sigma(nprof)
+      INTEGER,     INTENT(in)    ::  niter
+      INTEGER,     INTENT(inout) ::  iflag
+      
+!-----------------------------------------------------------------------
+!     Local Variables
+!        lreset_s    Gets set to true if using R,PHI,Z Specification
+!        ik          Dummy index
+!        ier         Error Flag
+!        iota_val    Holds profile evaulation
+!-----------------------------------------------------------------------
+      LOGICAL ::  lreset_s = .true.
+      INTEGER ::  ik, ier
+      REAL(rprec) :: iota_val, s11, s12, s21, s22
+!----------------------------------------------------------------------
+!     BEGIN SUBROUTINE
+!----------------------------------------------------------------------
+      IF (iflag < 0 ) RETURN
+      ik = COUNT(sigma < bigno)
+      IF (iflag == 1) WRITE(iunit_out,'(A,2(2X,I3.3))') 'S12 ',ik,4
+      IF (iflag == 1) WRITE(iunit_out,'(A)') 'TARGET  SIGMA  S12  S'
+      IF (niter >= 0) THEN
+         ! GET s if necessary
+         DO ik = 1, nprof
+            IF (sigma(ik) >= bigno) CYCLE
+            IF (s_s12(ik) <= 1.0 .and. s_s12(ik) >= 0.0) THEN
+               ier = 0
+               CALL get_equil_sus(s_s12(ik),s11,s12,s21,s22,ier)
+            ELSE
+               s12 = 0.0
+            END IF
+            mtargets = mtargets + 1
+            targets(mtargets) = target(ik)
+            sigmas(mtargets)  = sigma(ik)
+            vals(mtargets)    = s12
+            IF (iflag == 1) WRITE(iunit_out,'(4ES22.12E3)') target(ik),sigma(ik),s12,s_s12(ik)
+         END DO
+      ELSE
+         DO ik = 1, nprof
+            IF (sigma(ik) < bigno) THEN
+               mtargets = mtargets + 1
+               IF (niter == -2) target_dex(mtargets) = jtarget_s12
+            END IF
+         END DO
+      END IF
+      RETURN
+!----------------------------------------------------------------------
+!     END SUBROUTINE
+!----------------------------------------------------------------------
+      END SUBROUTINE chisq_s12
---
+++ STELLOPTV2/Sources/Chisq/chisq_s21.f90
@@ -0,0 +1,72 @@
+!-----------------------------------------------------------------------
+!     Subroutine:    chisq_s21
+!     Authors:       S. Lazerson (lazerson@pppl.gov)
+!     Date:          08/02/2019
+!     Description:   Calculates the S21 susceptance matrix target.
+!                    For details see
+!                       Strand and Houlberg, Phys. Plasmas 2001, Vol 8,
+!                              No. 6, p. 2782
+!-----------------------------------------------------------------------
+      SUBROUTINE chisq_s21(target,sigma,niter,iflag)
+!-----------------------------------------------------------------------
+!     Libraries
+!-----------------------------------------------------------------------
+      USE stellopt_runtime
+      USE stellopt_targets
+      USE equil_utils
+      
+!-----------------------------------------------------------------------
+!     Input/Output Variables
+!
+!-----------------------------------------------------------------------
+      REAL(rprec), INTENT(in)    ::  target(nprof)
+      REAL(rprec), INTENT(in)    ::  sigma(nprof)
+      INTEGER,     INTENT(in)    ::  niter
+      INTEGER,     INTENT(inout) ::  iflag
+      
+!-----------------------------------------------------------------------
+!     Local Variables
+!        lreset_s    Gets set to true if using R,PHI,Z Specification
+!        ik          Dummy index
+!        ier         Error Flag
+!        iota_val    Holds profile evaulation
+!-----------------------------------------------------------------------
+      LOGICAL ::  lreset_s = .true.
+      INTEGER ::  ik, ier
+      REAL(rprec) :: iota_val, s11, s12, s21, s22
+!----------------------------------------------------------------------
+!     BEGIN SUBROUTINE
+!----------------------------------------------------------------------
+      IF (iflag < 0 ) RETURN
+      ik = COUNT(sigma < bigno)
+      IF (iflag == 1) WRITE(iunit_out,'(A,2(2X,I3.3))') 'S21 ',ik,4
+      IF (iflag == 1) WRITE(iunit_out,'(A)') 'TARGET  SIGMA  S21  S'
+      IF (niter >= 0) THEN
+         ! GET s if necessary
+         DO ik = 1, nprof
+            IF (sigma(ik) >= bigno) CYCLE
+            IF (s_s21(ik) <= 1.0 .and. s_s21(ik) >= 0.0) THEN
+               ier = 0
+               CALL get_equil_sus(s_s21(ik),s11,s12,s21,s22,ier)
+            ELSE
+               s21 = 0.0
+            END IF
+            mtargets = mtargets + 1
+            targets(mtargets) = target(ik)
+            sigmas(mtargets)  = sigma(ik)
+            vals(mtargets)    = s21
+            IF (iflag == 1) WRITE(iunit_out,'(4ES22.12E3)') target(ik),sigma(ik),s21,s_s21(ik)
+         END DO
+      ELSE
+         DO ik = 1, nprof
+            IF (sigma(ik) < bigno) THEN
+               mtargets = mtargets + 1
+               IF (niter == -2) target_dex(mtargets) = jtarget_s21
+            END IF
+         END DO
+      END IF
+      RETURN
+!----------------------------------------------------------------------
+!     END SUBROUTINE
+!----------------------------------------------------------------------
+      END SUBROUTINE chisq_s21
