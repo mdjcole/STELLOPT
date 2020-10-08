d17d9edde3e9f4e5758f0c6e28d8b956c6885820
lazersos <samuel.lazerson@ipp.mpg.de>
2020-10-08 10:18:52 +0200

    BEAMS3D: Fixed bug where particles possibly followed beyond space when beams and not depo.

--- BEAMS3D/Sources/out_beams3d_nag.f90
+++ BEAMS3D/Sources/out_beams3d_nag.f90
@@ -168,6 +168,7 @@ SUBROUTINE out_beams3d_nag(t, q)
     END IF
     IF (lhitonly) mytdex = 0
     IF (ABS((t+dt)) .gt. ABS(mytdex*dt_out)) mytdex = mytdex + 1
+    IF (mytdex > npoinc) t = t_end(myline) ! This will end the run
     t = t + dt
 
     RETURN
